8ebd8e98e33a408037844c924ea59235505f215a
Mari Hansen Asplem <mariasplem@gmail.com>
2016-09-21 20:35:38 +0200

    Stripped down even more.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,10 @@ function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
+		--exclude ".macos" \
+		--exclude "init/" \
 		--exclude "bootstrap.sh" \
+		--exclude "brew.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
